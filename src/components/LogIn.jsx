5b8bc7d9740a7d0c19080634d541bda31ccb6c70
Kyle Kim <kylekimdoh@gmail.com>
2021-09-17 19:59:33 -0400

    react components boilerplate and login skeleton complete

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../stylesheets/styles.css';
+
+const LogIn = (props) => {
+
+  return (
+    <div>
+      Username: <input type ="text" /><br />
+      Password: <input type="password" /><br />
+      <button>Sign up
+
+      </button>
+      <button>Log in
+
+      </button>
+    </div>
+  )
+};
+
+export default LogIn;
