d9b519fff613a7d1b4d537c92d434cd99ce4883d
Hemwatie Persaud <hemwatiepersaud@gmail.com>
2021-09-17 21:15:47 -0400

    connected server to mongoDB database

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const path = require('path');
 const express = require('express');
-
+const databaseController = require('./controllers/databaseController');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
+
+const { json } = require('express');
+// const fetch = require('node-fetch');
+// import fetch from 'node-fetch';
+const fs = require('fs');
+
+app.get('/', databaseController.showData ,(req,res) => {
   res.send('hello');
+
 })
 
 app.use(express.static(path.resolve(__dirname, '../src')));
+
 
 
 app.listen(port, () => {
